dd291f6f377edf76914fdba8435523a3aa934bbf
cameronwp <cameron.w.pittman@gmail.com>
2017-04-23 13:45:36 -0500

    add working example with hardcoded subfields

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ndjson-to-csv
 Convert newline delineated JSON to a CSV for easy consumption.
+
+## Usage
+
+Install.
+
+```sh
+npm i
+```
+
+Run.
+
+```sh
+node . path/to/input.txt path/to/output.csv
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const csvWriter = require('csv-write-stream')
+const fs = require('fs');
+const ndjson = require('ndjson');
+
+const args = process.argv.slice(2);
+
+let inputFilePath = args[0];
+let outputFilePath = args[1];
+
+if (!inputFilePath || !outputFilePath) {
+  console.error('Usage is:\n\nnode . path/to/input.txt path/to/output.csv');
+  process.exit(1);
+}
+
+console.log(`Reading NDJSON from ${inputFilePath}.`);
+
+const writer = csvWriter({headers: [
+  '_id',
+  'pet',
+  'petRemaining',
+  'bestPetEstimate',
+  'petMargin',
+  'timelineMargin',
+  'stepId',
+  'channel',
+  'type',
+  'description',
+  'action',
+]})
+writer.pipe(fs.createWriteStream(outputFilePath));
+
+fs.createReadStream(inputFilePath)
+  .pipe(ndjson.parse({strict: false}))
+  .on('data', data => {
+    const [key] = Object.keys(data);
+    if (key === 'docs') {
+      data[key].forEach(d => writer.write(d));
+    }
+  })
+  .on('end', () => {
+    writer.end();
+    console.log(`Finished writing ${outputFilePath}`);
+    process.exit(0);
+  });
